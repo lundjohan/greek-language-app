d1f6f76e73402828999303a49eede22aa468a1b3
Johan Lund <drancisdrake@gmail.com>
2021-02-16 10:57:42 +0100

    collapsing toolbar (greek flag) - almost there

--- app/src/main/java/lund/johan/greeklanguageapp/chapterItems/ChapterItemsFragment.kt
+++ app/src/main/java/lund/johan/greeklanguageapp/chapterItems/ChapterItemsFragment.kt
@@ -6,10 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import lund.johan.greeklanguageapp.MainActivity
 import lund.johan.greeklanguageapp.R
 import lund.johan.greeklanguageapp.databinding.FragmentChapterItemsBinding
 
 class ChapterItemsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //(requireActivity() as MainActivity).supportActionBar!!.hide()
+
+
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,15 +26,17 @@ class ChapterItemsFragment : Fragment() {
         val binding: FragmentChapterItemsBinding =
             FragmentChapterItemsBinding.inflate(inflater, container, false)
 
+
+
         //initiate RecyclerView
-          //line below will be replaced by repository
+        //line below will be replaced by repository
         val dataset: Array<ChapterTeaser> = arrayOf(
-            ChapterTeaser(R.drawable.acropolis_5_3,"The Acropolis"),
+            ChapterTeaser(R.drawable.acropolis_5_3, "The Acropolis"),
             ChapterTeaser(R.drawable.greek_food_5_3, "Greek Food"),
             ChapterTeaser(R.drawable.mycenae_5_3, "Mycenae")
         )
 
-        val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(context)
+        val linearLayoutManager = LinearLayoutManager(context)
         binding.chapterItems.layoutManager = linearLayoutManager
         val adapter: ChapterItemsAdapter = ChapterItemsAdapter(this, dataset)
         binding.chapterItems.adapter = adapter
