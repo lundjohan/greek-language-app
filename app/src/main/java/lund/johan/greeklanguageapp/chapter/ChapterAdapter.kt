34a965c01464471d548e01aff8b9d49a36ca6c0a
Johan Lund <drancisdrake@gmail.com>
2021-02-19 13:32:47 +0100

    video is playing

--- app/src/main/java/lund/johan/greeklanguageapp/chapter/ChapterAdapter.kt
+++ app/src/main/java/lund/johan/greeklanguageapp/chapter/ChapterAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
 import lund.johan.greeklanguageapp.R
 import lund.johan.greeklanguageapp.repository.Repository
@@ -20,15 +21,16 @@ class ChapterAdapter (
         val chapterInfo: TextView = view.findViewById(R.id.info_long_text);
 
         init {
+            view.setOnClickListener{navigateToScreen(smallIds[adapterPosition])}
             //chapterImg.setOnClickListener{ navigateToScreen(smallIds[adapterPosition])}
             //chapterInfo.setOnClickListener{ navigateToScreen(smallIds[adapterPosition])}
         }
     }
 
-    //this has to change
-    fun navigateToScreen(chapterId: Int) {
-        //val action = ChapterItemsFragmentDirections.actionChaptersToChapter(chapterId);
-        //NavHostFragment.findNavController(usingFragment).navigate(action);
+    //this has to change => now every item is going to same video.
+    fun navigateToScreen(id: Int) {
+        val action = ChapterFragmentDirections.actionChapterToVideoplayer(usingFragment.getString(R.string.video_url));
+        NavHostFragment.findNavController(usingFragment).navigate(action);
     }
 
     // Create new views (invoked by the layout manager)
---
+++ app/src/main/java/lund/johan/greeklanguageapp/video/VideoPlayerFragment.kt
@@ -0,0 +1,97 @@
+package lund.johan.greeklanguageapp.video
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.util.Util
+import lund.johan.greeklanguageapp.databinding.FragmentVideoPlayerBinding
+
+class VideoPlayerFragment : Fragment() {
+    lateinit var videoUrl: String
+
+    lateinit var playerView: PlayerView
+    private var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        videoUrl = VideoPlayerFragmentArgs.fromBundle(requireArguments()).videoUrl
+        val binding =
+            FragmentVideoPlayerBinding.inflate(inflater, container, false)
+        playerView = binding.videoView
+        return binding.root
+    }
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+        playerView.player = player
+        val mediaItem = MediaItem.fromUri(videoUrl)
+        player!!.setMediaItem(mediaItem)
+        player!!.setPlayWhenReady(playWhenReady)
+        player!!.seekTo(currentWindow, playbackPosition)
+        player!!.prepare()
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playbackPosition = player!!.getCurrentPosition()
+            currentWindow = player!!.getCurrentWindowIndex()
+            playWhenReady = player!!.getPlayWhenReady()
+            player!!.release()
+            player = null
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+}
